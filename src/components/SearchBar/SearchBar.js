f81519fbbadaf590faa6b4d46e608e49ef2b3d8f
moonshoeskelly <45537465+moonshoeskelly@users.noreply.github.com>
2025-07-25 19:50:10 -0800

    Add styling to search bar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './SearchBar.module.css';
 
 const SearchBar = () => {
 
+    const sortOptions = [
+    { value: "best_match", option: "Best Match" },
+    { value: "rating", option: "Highest Rated" },
+    { value: "review_count", option: "Most Reviewed" }
+  ];
+
+    const [activeButton, setActiveButton] = useState(null);
+
+    const sortingClick = (buttonId) => {
+    setActiveButton(buttonId);
+  };
+
     return (
         <div className={styles.SearchBar}>
-            <input id="business-type" type="text" placeholder="Search Business?"/>
-            <input id="location" type="text" placeholder="Where?" />
-            <button id="submit" type="submit">
+            <div className={styles.SearchBarSorting}>
+                {sortOptions.map((option) => (
+                <button key={option.value} onClick={() => sortingClick(option.value)} 
+                className={`${activeButton === option.value ? styles.active : ''}`}>
+                {option.option}</button>
+                ))}
+            </div>
+            <div className={styles.SearchBarInputs}>
+                <input id={styles["business-type"]} type="text" placeholder="Search Business?"/>
+                <input id={styles["location"]} type="text" placeholder="Where?" />
+            </div>
+            <button className={styles.Submit} type="submit">
             Let's Go
             </button>
         </div>
