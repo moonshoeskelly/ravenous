f194a6ff1c9018f3737b0f29b9c265404ddad5cf
moonshoeskelly <45537465+moonshoeskelly@users.noreply.github.com>
2025-08-07 15:41:41 -0800

    Add functionality to Search Bar inputs

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,31 +4,60 @@ import styles from './SearchBar.module.css';
 const SearchBar = () => {
 
     const sortOptions = [
-    { value: "best_match", option: "Best Match" },
-    { value: "rating", option: "Highest Rated" },
-    { value: "review_count", option: "Most Reviewed" }
-  ];
+        { value: "best_match", option: "Best Match" },
+        { value: "rating", option: "Highest Rated" },
+        { value: "review_count", option: "Most Reviewed" }
+    ];
 
-    const [activeButton, setActiveButton] = useState(null);
+    const [activeButton, setActiveButton] = useState(sortOptions[0].value);
+    const [sortOption, setSortOption] = useState("best_match");
+    const [searchTerm, setSearchTerm] = useState('');
+    const [location, setLocation] = useState('');
 
-    const sortingClick = (buttonId) => {
-    setActiveButton(buttonId);
-  };
+    const handleSortButtonClick = (clickEvent) => {
+      const selectedValue = clickEvent.target.value;
+      sortingClick(selectedValue);
+    };
+
+    const sortingClick = (selectedSortValue) => {
+        setActiveButton(selectedSortValue);
+        sortOptionSelect(selectedSortValue);
+    };
+
+    const sortOptionSelect = (selectedSortValue) => {
+      setSortOption(selectedSortValue);
+    };
+
+    const searchTermInput = (inputEvent) => {
+        setSearchTerm(inputEvent.target.value);
+    }
+
+    const locationInput = (inputEvent) => {
+        setLocation(inputEvent.target.value);
+    }
+
+    const handleSearch = () => {
+      console.log(`Searching Yelp with ${searchTerm}, ${location}, ${sortOption}`);
+    };
 
     return (
         <div className={styles.SearchBar}>
             <div className={styles.SearchBarSorting}>
                 {sortOptions.map((option) => (
-                <button key={option.value} onClick={() => sortingClick(option.value)} 
-                className={`${activeButton === option.value ? styles.active : ''}`}>
-                {option.option}</button>
+                <button 
+                    key={option.value} 
+                    value={option.value}
+                    onClick={handleSortButtonClick}
+                    className={activeButton === option.value ? styles.active : ''}>
+                    {option.option}
+                </button>
                 ))}
             </div>
             <div className={styles.SearchBarInputs}>
-                <input id={styles["business-type"]} type="text" placeholder="Search Business?"/>
-                <input id={styles["location"]} type="text" placeholder="Where?" />
+                <input id={styles["business-type"]} type="text" placeholder="Search Business?" value={searchTerm} onInput={searchTermInput}/>
+                <input id={styles["location"]} type="text" placeholder="Where?" value={location} onInput={locationInput}/>
             </div>
-            <button className={styles.Submit} type="submit">
+            <button className={styles.Submit} type="submit" onClick={handleSearch}>
             Let's Go
             </button>
         </div>
